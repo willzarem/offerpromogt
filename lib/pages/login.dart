b7d5d1fd204b7bcd87aee399e5c50af5951e2c9a
willzarem <willmer.hg@gmail.com>
2019-11-10 20:36:03 -0600

    fixes for facebook login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:offerpromogt/pages/home.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class Login extends StatefulWidget {
   Login({Key key}) : super(key: key);
@@ -9,6 +12,60 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+
+  static final FacebookLogin facebookSignIn = new FacebookLogin();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  FirebaseUser firebaseUser;
+
+  String _message = 'Log in/out by pressing the buttons below.';
+
+  Future<FirebaseUser> firebaseAuthWithFacebook({@required FacebookAccessToken token}) async {
+
+    AuthCredential credential= FacebookAuthProvider.getCredential(accessToken: token.token);
+    AuthResult authResult = await firebaseAuth.signInWithCredential(credential);
+    return authResult.user;
+  }
+
+  Future<void> _login() async {
+    final FacebookLoginResult result =
+        await facebookSignIn.logIn(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final FacebookAccessToken accessToken = result.accessToken;
+        _showMessage('''
+         Logged in!
+         
+         Token: ${accessToken.token}
+         User id: ${accessToken.userId}
+         Expires: ${accessToken.expires}
+         Permissions: ${accessToken.permissions}
+         Declined permissions: ${accessToken.declinedPermissions}
+         ''');
+         firebaseUser = await firebaseAuthWithFacebook(token: accessToken);
+         print(_message);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        _showMessage('Login cancelled by the user.');
+        break;
+      case FacebookLoginStatus.error:
+        _showMessage('Something went wrong with the login process.\n'
+            'Here\'s the error Facebook gave us: ${result.errorMessage}');
+        break;
+    }
+  }
+
+  Future<Null> _logOut() async {
+    await facebookSignIn.logOut();
+    _showMessage('Logged out.');
+  }
+
+  void _showMessage(String message) {
+    setState(() {
+      _message = message;
+    });
+  }
 
   TextEditingController _passwordController = TextEditingController();
   TextEditingController _usernameController = TextEditingController();
@@ -20,7 +77,7 @@ class _LoginState extends State<Login> {
       body: Center(
         child: Container(
           alignment: Alignment.center,
-          height: 480.0,
+          height: 550.0,
           width: 250.0,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -70,6 +127,17 @@ class _LoginState extends State<Login> {
                         },
                         child: Text(
                           'Entrar',
+                          style: TextStyle(fontSize: 19.0),
+                        ),
+                      ),
+                      RaisedButton(
+                        color: Color(0xFF3b5998),
+                        textColor: Colors.white,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        onPressed: _login,
+                        child: Text(
+                          'Facebook',
                           style: TextStyle(fontSize: 19.0),
                         ),
                       ),
---
+++ lib/pages/main3.dart
@@ -0,0 +1,258 @@
+// import 'dart:async';
+
+// import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/foundation.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => new _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   static final FacebookLogin facebookSignIn = new FacebookLogin();
+
+//   //my code
+//   final FirebaseAuth _authInstance = FirebaseAuth.instance;
+//   //end my code
+
+//   Future<FirebaseUser> firebaseAuthWithFacebook({@required FacebookAccessToken token}) async {
+
+//     AuthCredential credential= FacebookAuthProvider.getCredential(accessToken: token.token);
+//     AuthResult authResult = await _authInstance.signInWithCredential(credential);
+//     return authResult.user;
+//   }
+  
+    
+
+//   Future<Null> _signOut(BuildContext context) async {
+//     await facebookSignIn.logOut();
+//     Scaffold.of(context).showSnackBar(new SnackBar(
+//           content: new Text('Sign out button clicked'),
+//         ));
+//     print('Signed out');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       home: new Scaffold(
+//         appBar: new AppBar(
+//           title: new Text('Fb Sign In with Firebase'),
+//         ),
+//         body: new Builder(
+//           builder: (BuildContext context) {
+//             return new Center(
+//               child: new Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   new MaterialButton(
+//                     //padding: new EdgeInsets.all(16.0),
+//                     minWidth: 150.0,
+//                     onPressed: () async {
+//                       final FacebookLoginResult facebookLoginResult = await facebookSignIn.logInWithReadPermissions(['email', 'public_profile']);
+
+//                         switch (facebookLoginResult.status) {
+//                           case FacebookLoginStatus.error:
+//                             print("Error");
+//                             break;
+
+//                           case FacebookLoginStatus.cancelledByUser:
+//                             print("CancelledByUser");
+//                             break;
+
+//                           case FacebookLoginStatus.loggedIn:
+//                             print("LoggedIn");
+//                             /// calling the auth mehtod and getting the logged user
+//                             var firebaseUser = await firebaseAuthWithFacebook(
+//                                 token: facebookLoginResult.accessToken);
+//                             print(firebaseUser);
+//                         }
+//                     },
+//                     child: new Text('Sign in with Facebook'),
+//                     color: Colors.lightBlueAccent,
+//                   ),
+//                   new Padding(
+//                     padding: const EdgeInsets.all(5.0),
+//                   ),
+//                   new MaterialButton(
+//                     minWidth: 150.0,
+//                     onPressed: () => _signOut(context),
+//                     child: new Text('Sign Out'),
+//                     color: Colors.lightBlueAccent,
+//                   ),
+//                 ],
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class UserInfoDetails {
+//   UserInfoDetails(this.providerId, this.uid, this.displayName, this.photoUrl,
+//       this.email, this.isAnonymous, this.isEmailVerified, this.providerData);
+
+//   /// The provider identifier.
+//   final String providerId;
+
+//   /// The provider’s user ID for the user.
+//   final String uid;
+
+//   /// The name of the user.
+//   final String displayName;
+
+//   /// The URL of the user’s profile photo.
+//   final String photoUrl;
+
+//   /// The user’s email address.
+//   final String email;
+
+//   // Check anonymous
+//   final bool isAnonymous;
+
+//   //Check if email is verified
+//   final bool isEmailVerified;
+
+//   //Provider Data
+//   final List<ProviderDetails> providerData;
+// }
+
+// class ProviderDetails {
+//   final String providerId;
+
+//   final String uid;
+
+//   final String displayName;
+
+//   final String photoUrl;
+
+//   final String email;
+
+//   ProviderDetails(
+//       this.providerId, this.uid, this.displayName, this.photoUrl, this.email);
+// }
+
+// class DetailedScreen extends StatelessWidget {
+//   final UserInfoDetails detailsUser;
+
+//   // DetailScreen({
+//   //   Key key,
+//   //   this.name}) : super(key: key);
+
+//   DetailedScreen({Key key, @required this.detailsUser}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//         appBar: new AppBar(
+//           title: new Text('Fb Sign In Details'),
+//           automaticallyImplyLeading: false,
+//         ),
+//         body: new Container(
+//           padding: const EdgeInsets.all(10.0),
+//           child: new Column(
+//             children: <Widget>[
+//               new Text(
+//                 "Name : " + detailsUser.displayName,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "Email : " + detailsUser.email,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "isAnonymous : " + detailsUser.isAnonymous.toString(),
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "isEmailVerified : " + detailsUser.isEmailVerified.toString(),
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "Photo URL : " + detailsUser.photoUrl,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "Provider ID : " + detailsUser.providerId,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "UiD : " + detailsUser.uid,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(
+//                 padding: new EdgeInsets.all(5.0),
+//               ),
+//               new Text(
+//                 "Fb provider data : " +
+//                     detailsUser.providerData[0].displayName +
+//                     " : " +
+//                     detailsUser.providerData[0].uid,
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.ellipsis,
+//                 textScaleFactor: 1.2,
+//                 style: new TextStyle(
+//                     fontWeight: FontWeight.bold, color: Colors.brown),
+//               ),
+//               new Padding(padding: const EdgeInsets.all(10.0)),
+//               new Image.network(
+//                 detailsUser.photoUrl,
+//                 colorBlendMode: BlendMode.dstIn,
+//               )
+//             ],
+//           ),
+//         ));
+//   }
+// }
