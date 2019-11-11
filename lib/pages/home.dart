3d5f1ced8c9c84c79676e8b0383b75556d6cb711
Willmer Hernandez <whernandez@popoyan.com>
2019-11-10 19:54:51 -0600

    Added firestore instance and hardcoded user ID

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,10 @@ import 'package:flutter_svg/svg.dart';
 import 'package:offerpromogt/models/coupon.dart';
 import 'package:offerpromogt/pages/coupons_list.dart';
 import 'package:offerpromogt/pages/profile.dart';
+import 'package:offerpromogt/utils/firestore.dart';
+
+final String userDocId = 'n5cHzFOKMN0KLddc1p7w';
+final FirestoreService firestore = FirestoreService();
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
