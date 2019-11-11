314d9c7a56c3b121119d214bd5f4dbe0a43de627
Willmer Hernandez <whernandez@popoyan.com>
2019-11-11 03:54:44 -0600

    Updated user model with missing fields

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,20 +1,37 @@
 class User {
   String docId;
-  String fullName;
+  String displayName;
+  String email;
+  String photoUrl;
+  String fbId;
   DateTime birthDate;
   String idNumber;
 
-  User({this.docId, this.fullName, this.birthDate, this.idNumber});
+  User(
+      {this.docId,
+      this.displayName,
+      this.birthDate,
+      this.idNumber,
+      this.email,
+      this.fbId,
+      this.photoUrl});
 
   factory User.fromMap(Map<String, dynamic> map, {String docId}) => User(
-      docId: docId,
-      fullName: map['fullName'],
-      birthDate: map['birthDate'].toDate(),
-      idNumber: map['idNumber']);
+        docId: docId,
+        displayName: map['displayName'],
+        birthDate: map['birthDate']?.toDate(),
+        idNumber: map['idNumber'],
+        email: map['email'],
+        fbId: map['fbId'],
+        photoUrl: map['photoUrl'],
+      );
 
   Map<String, dynamic> toMap() => {
-        'fullName': this.fullName,
+        'displayName': this.displayName,
         'birthDate': this.birthDate,
         'idNumber': this.idNumber,
+        'email': this.email,
+        'fbId': this.fbId,
+        'photoUrl': this.photoUrl,
       };
 }
